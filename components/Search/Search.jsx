1bb065456cb842623f9dbf31ea33de094e24d4ee
James <jamestariga08@live.com>
2022-07-16 17:49:54 -0700

    ✨ Minor update

--- components/Search/Search.jsx
+++ components/Search/Search.jsx
@@ -1,14 +1,20 @@
 import { View, TextInput } from 'react-native'
+import { useState } from 'react'
 import { SearchIcon, AdjustmentsIcon } from 'react-native-heroicons/outline'
 
 const Search = () => {
+  const [search, setSearch] = useState('')
+
   return (
-    <View className='flex-row items-center space-x-2 px-4 pb-2 mx-4'>
-      <View className='flex-row flex-1 space-x-2 bg-gray-200 p-3'>
+    <View className='flex-row items-center space-x-2 px-2 pb-2 mx-4'>
+      <View className='flex-row flex-1 space-x-2 bg-gray-200 p-3 rounded-xl'>
         <SearchIcon color='#00CCBB' />
         <TextInput
+          className='flex-1 text-blue-400 placeholder:text-black'
           placeholder='Restaurants and Cuisines'
           keyboardType='default'
+          defaultValue={search}
+          onChangeText={(text) => setSearch(text)}
         />
       </View>
       <AdjustmentsIcon size={20} color='#00CCBB' />
