cc72bdaeb50f1c3720d37ba2ac0a24f0e2c465b6
James <jamestariga08@live.com>
2022-07-16 17:52:05 -0700

    ✨ Implemented categories and featured components

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -1,9 +1,18 @@
 import { useLayoutEffect } from 'react'
-import { SafeAreaView, View, Text, Image } from 'react-native'
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Image,
+  ScrollView,
+  Platform,
+} from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import logo from '../../assets/images/logo.png'
 import { UserIcon, ChevronDownIcon } from 'react-native-heroicons/outline'
 import Search from '../../components/Search/Search'
+import Categories from '../../components/Categories/Categories'
+import Featured from '../../components/Featured/Featured'
 
 const Home = () => {
   const navigation = useNavigation()
@@ -14,27 +23,46 @@ const Home = () => {
     })
   }, [])
 
+  // Bottom padding for Android
+  const androidBottom = 150
+
+  // Bottom padding for iOS
+  const iosBottom = 100
+
   return (
     <SafeAreaView className='bg-indigo-500 pt-5'>
-      <Text className='text-red-500'>
-        <View className='flex-row pb-3 items-center mx-4 px-4 space-x-2'>
-          <Image
-            source={logo}
-            className='h-7 w-7 p-4 rounded-full bg-gray-300'
-          />
-          <View className='flex-1'>
-            <Text className='font-bold text-gray-400 text-xs'>
-              Deliver Now!
-            </Text>
-            <Text className='font-bold text-xl'>
-              Current Location
-              <ChevronDownIcon size={20} color='#00CCBB' />
-            </Text>
-          </View>
-          <UserIcon size={35} color='#00CCBB' />
+      <View className='flex-row pb-3 items-center mx-4 px-2 space-x-2'>
+        <Image source={logo} className='h-7 w-7 p-4 rounded-full bg-gray-300' />
+        <View className='flex-1'>
+          <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
+          <Text className='font-bold text-xl'>
+            Current Location
+            <ChevronDownIcon size={20} color='#00CCBB' />
+          </Text>
         </View>
-        <Search />
-      </Text>
+        <UserIcon size={35} color='#00CCBB' />
+      </View>
+      <Search />
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: Platform.OS === 'android' ? androidBottom : iosBottom,
+        }}
+        className='bg-gray-100'
+      >
+        <Categories />
+        <Featured
+          title='Featured'
+          description='Paid placements from our partners'
+        />
+        <Featured
+          title='Tasty Discounts'
+          description="Everyone's been enjoying these juicy discounts!"
+        />
+        <Featured
+          title='Offers near you!'
+          description='Why not support your local restaurants!'
+        />
+      </ScrollView>
     </SafeAreaView>
   )
 }
