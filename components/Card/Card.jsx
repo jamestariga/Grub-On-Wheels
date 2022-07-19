a419ea4b030f24e53fe0226d5cc43396f70fc056
James <jamestariga08@live.com>
2022-07-18 18:38:24 -0700

    ✨ Displayed the the categories and featured restaurants images

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,12 +1,17 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { Text, Image, TouchableOpacity } from 'react-native'
+import { urlFor } from '../../sanity'
 
 const Card = (props) => {
   const { img, title } = props
 
   return (
     <TouchableOpacity className='relative mr-2'>
-      <Image source={img} className='h-20 w-20 rounded' />
+      <Image
+        source={{
+          uri: urlFor(img).url(),
+        }}
+        className='h-20 w-20 rounded'
+      />
       <Text className='absolute bottom-1 left-1 text-white font-bold'>
         {title}
       </Text>
--- components/Card/RestaurantCard.jsx
+++ components/Card/RestaurantCard.jsx
@@ -1,6 +1,7 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import { StarIcon } from 'react-native-heroicons/solid'
 import { LocationMarkerIcon } from 'react-native-heroicons/outline'
+import { urlFor } from '../../sanity'
 
 const RestaurantCard = (props) => {
   const {
@@ -18,18 +19,23 @@ const RestaurantCard = (props) => {
 
   return (
     <TouchableOpacity className='bg-white mr-3 shadow'>
-      <Image source={img} className='h-36 w-64 rounded-sm' />
+      <Image
+        source={{
+          uri: urlFor(img).url(),
+        }}
+        className='h-36 w-64 rounded-sm'
+      />
       <View className='px-3 pb-4'>
         <Text className='font-bold text-lg pt-2'>{title}</Text>
         <View className='flex-row items-center space-x-1'>
           <StarIcon size={22} color='green' opacity={0.5} />
           <Text className='text-xs text-gray-500'>
-            <Text className='text-green-500'>{rating}</Text> .{genre}
+            <Text className='text-green-500'>{rating}</Text> · {genre}
           </Text>
         </View>
         <View className='flex-row items-center space-x-1'>
           <LocationMarkerIcon size={22} color='gray' opacity={0.4} />
-          <Text className='text-xs text-gray-500'>Nearby . {address}</Text>
+          <Text className='text-xs text-gray-500'>Nearby · {address}</Text>
         </View>
       </View>
     </TouchableOpacity>
