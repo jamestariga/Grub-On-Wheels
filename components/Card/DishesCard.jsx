c7ab71290d913cba08b1f1c534e62dbe93536cfa
James <jamestariga08@live.com>
2022-07-20 15:34:16 -0700

    ✨ Created a card for the dishes

---
+++ components/Card/DishesCard.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { urlFor } from '../../sanity'
+import Currency from 'react-currency-formatter'
+import { MinusCircleIcon, PlusCircleIcon } from 'react-native-heroicons/solid'
+
+const DishesCard = (props) => {
+  const { id, image, name, description, price } = props
+  const [isPressed, setIsPressed] = useState(false)
+
+  return (
+    <>
+      <TouchableOpacity
+        onPress={() => setIsPressed(!isPressed)}
+        className={`bg-white border p-4 border-gray-200 ${
+          isPressed && 'border-b-0'
+        }`}
+      >
+        <View className='flex-row'>
+          <View className='flex-1 pr-2'>
+            <Text className='text-lg mb-1'>{name}</Text>
+            <Text className='text-gray-400'>{description}</Text>
+            <Text className='text-gray-400 mt-2'>
+              <Currency quantity={price} currency='CAD' />
+            </Text>
+          </View>
+
+          <View>
+            <Image
+              style={{
+                borderWidth: 1,
+                borderColor: '#F3F3F4',
+              }}
+              source={{ uri: urlFor(image).url() }}
+              className='h-20 w-20 bg-gray-300 p-4'
+            />
+          </View>
+        </View>
+      </TouchableOpacity>
+
+      {isPressed && (
+        <View className='bg-white px-4'>
+          <View className='flex-row items-center space-x-2 pb-3'>
+            <TouchableOpacity>
+              <MinusCircleIcon
+                size={40}
+                // color={items.length > 0 ? '#00CCBB' : 'gray'}
+                color='#00CCBB'
+              />
+            </TouchableOpacity>
+            <Text>0</Text>
+            <TouchableOpacity>
+              <PlusCircleIcon
+                size={40}
+                // color={items.length > 0 ? '#00CCBB' : 'gray'}
+                color='#00CCBB'
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
+    </>
+  )
+}
+
+export default DishesCard
