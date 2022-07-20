7087c4e42fc13b2cf16a1e6e43ebfdf854a1c610
James <jamestariga08@live.com>
2022-07-20 15:29:33 -0700

    ✨ Added a new screen

--- App.jsx
+++ App.jsx
@@ -2,6 +2,8 @@ import { TailwindProvider } from 'tailwindcss-react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from './pages/Home/Home'
+import Restaurant from './pages/Restaurant/Restaurant'
+import Category from './pages/Category/Category'
 
 const Stack = createNativeStackNavigator()
 
@@ -11,6 +13,8 @@ const App = () => {
       <TailwindProvider>
         <Stack.Navigator>
           <Stack.Screen name='HomePage' component={Home} />
+          <Stack.Screen name='RestaurantPage' component={Restaurant} />
+          <Stack.Screen name='CategoryPage' component={Category} />
         </Stack.Navigator>
       </TailwindProvider>
     </NavigationContainer>
