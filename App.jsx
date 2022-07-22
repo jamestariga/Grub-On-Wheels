12ba2dd2de9d66fecd922be5aefa1839add2e0c0
James <jamestariga08@live.com>
2022-07-21 19:27:51 -0700

    ✨ Implemented the Redux store

--- App.jsx
+++ App.jsx
@@ -4,19 +4,23 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from './pages/Home/Home'
 import Restaurant from './pages/Restaurant/Restaurant'
 import Category from './pages/Category/Category'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 const Stack = createNativeStackNavigator()
 
 const App = () => {
   return (
     <NavigationContainer>
-      <TailwindProvider>
-        <Stack.Navigator>
-          <Stack.Screen name='HomePage' component={Home} />
-          <Stack.Screen name='RestaurantPage' component={Restaurant} />
-          <Stack.Screen name='CategoryPage' component={Category} />
-        </Stack.Navigator>
-      </TailwindProvider>
+      <Provider store={store}>
+        <TailwindProvider>
+          <Stack.Navigator>
+            <Stack.Screen name='HomePage' component={Home} />
+            <Stack.Screen name='RestaurantPage' component={Restaurant} />
+            <Stack.Screen name='CategoryPage' component={Category} />
+          </Stack.Navigator>
+        </TailwindProvider>
+      </Provider>
     </NavigationContainer>
   )
 }
