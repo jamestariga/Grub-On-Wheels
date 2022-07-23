02ce76cbfbbce2a4db0dc99e3f463aa757843a84
James <jamestariga08@live.com>
2022-07-22 21:07:34 -0700

    ✨ Added navigation to the Order page

--- components/Cart/Cart.jsx
+++ components/Cart/Cart.jsx
@@ -9,9 +9,14 @@ const Cart = () => {
   const total = useSelector(selectCartTotal)
   const navigation = useNavigation()
 
+  if (items.length === 0) return null
+
   return (
     <View className='absolute bottom-10 w-full z-50'>
-      <TouchableOpacity className='flex-row items-center space-x-1 rounded-lg p-4 mx-5 bg-[#00CCBB]'>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('OrderPage')}
+        className='flex-row items-center space-x-1 rounded-lg p-4 mx-5 bg-[#00CCBB]'
+      >
         <Text className='text-white font-extrabold text-lg bg-[#01A296] py-1 px-2 rounded-lg'>
           {items.length}
         </Text>
