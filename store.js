8f1b329bd7a4435816e306f253e8517183438ded
James <jamestariga08@live.com>
2022-07-24 19:41:11 -0700

    ✨ Implemented the location reducer

--- store.js
+++ store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from './features/cartSlice'
 import restaurantReducer from './features/restaurantSlice'
+import locationReducer from './features/locationSlice'
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
     restaurant: restaurantReducer,
+    location: locationReducer,
   },
 })
